4fa68cf971cd47e84d05f6e53e22adcdc9f85533
ahmedaidev <ahmedaidev@protonmail.com>
2022-04-09 11:29:13 +0200

    DEV: Move values location

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
-helm install -f values/postgres-values.yaml postgres charts/postgres
-helm install -f values/restapi-values.yaml restapi charts/restapi
+helm install -f k8s/values/postgres-values.yaml postgres charts/postgres
+helm install -f k8s/values/restapi-values.yaml restapi charts/restapi
